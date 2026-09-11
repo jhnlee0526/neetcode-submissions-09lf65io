a1c0000b667df3a0693fa9fab130946180e2e349
Johann Lee <63811750+jhnlee0526@users.noreply.github.com>
2026-09-10 20:53:09 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = '' # 'count#word'
+
+        for word in strs:
+            res += f'{len(word)}#{word}'
+
+        return res # 5#Hello5#World
+
+
+    def decode(self, s: str) -> List[str]:
+        if not s: # edge case
+            return []
+        
+        # s -> 5#Hello5#World
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            
+            # j located at the '#'
+            length = int(s[i : j]) # from i to (j - 1)
+            word = s[j + 1 : j + 1 + length]
+            res.append(word)
+
+            i = j + 1 + length
+
+        return res
